a82786cd5c2115fbd386675bf146ac1fab4a7e06
Sam Mozer <sammy.mozer@gmail.com>
2025-04-14 14:41:45 -0500

    fix

--- api/index.py
+++ api/index.py
@@ -1,7 +1,12 @@
+# api/index.py
 from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def get_headers():
-    return dict(request.headers)
+    return request.headers
+
+# Required for Vercel's handler
+def handler(environ, start_response):
+    return app(environ, start_response)
